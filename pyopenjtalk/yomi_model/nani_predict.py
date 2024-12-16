e2c5435efc3c61fe478c5138fc86d6437b5d16e5
tsukumi <tsukumijima@users.noreply.github.com>
2024-12-17 04:14:38 +0900

    Fix: コードフォーマッタを適用

--- pyopenjtalk/yomi_model/nani_predict.py
+++ pyopenjtalk/yomi_model/nani_predict.py
@@ -11,8 +11,14 @@ model_dir = os.path.dirname(__file__)
 
 # ONNX モデルをロード
 # 非常に軽量なモデルのため、import 時に ONNX モデルをロードするオーバーヘッドはほとんどない
-enc_session = InferenceSession(os.path.join(model_dir, "nani_enc.onnx"), providers=["CPUExecutionProvider"])
-model_session = InferenceSession(os.path.join(model_dir, "nani_model.onnx"), providers=["CPUExecutionProvider"])
+enc_session = InferenceSession(
+    os.path.join(model_dir, "nani_enc.onnx"),
+    providers=["CPUExecutionProvider"],
+)
+model_session = InferenceSession(
+    os.path.join(model_dir, "nani_model.onnx"),
+    providers=["CPUExecutionProvider"],
+)
 
 
 def predict(input_njd: List[Union[NJDFeature, None]]) -> int:
