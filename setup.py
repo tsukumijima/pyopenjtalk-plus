4c25da69b2f7f785bdfd9efca503497890983214
tsukumi <tsukumijima@users.noreply.github.com>
2024-12-25 05:57:07 +0900

    Release: version 0.3.4-post9

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import Extension, find_packages, setup
 
 platform_is_windows = sys.platform == "win32"
 
-version = "0.3.4-post8"
+version = "0.3.4-post9"
 
 msvc_extra_compile_args_config = [
     "/source-charset:utf-8",
