7c484a6b099879b0119a54060054d565f1b66cbf
tsukumi <tsukumijima@users.noreply.github.com>
2025-01-25 05:10:51 +0900

    Refactor: NJDFeature の各フィールドにコメントを付与

--- pyopenjtalk/types.py
+++ pyopenjtalk/types.py
@@ -2,17 +2,19 @@ from typing import TypedDict
 
 
 class NJDFeature(TypedDict):
-    string: str
-    pos: str
-    pos_group1: str
-    pos_group2: str
-    pos_group3: str
-    ctype: str
-    cform: str
-    orig: str
-    read: str
-    pron: str
-    acc: int
-    mora_size: int
-    chain_rule: str
-    chain_flag: int
+    """OpenJTalk の形態素解析結果・アクセント推定結果を表す型"""
+
+    string: str  # 表層形
+    pos: str  # 品詞
+    pos_group1: str  # 品詞細分類1
+    pos_group2: str  # 品詞細分類2
+    pos_group3: str  # 品詞細分類3
+    ctype: str  # 活用型
+    cform: str  # 活用形
+    orig: str  # 原形
+    read: str  # 読み
+    pron: str  # 発音形式
+    acc: int  # アクセント型 (0: 平板型, 1-n: n番目のモーラにアクセント核)
+    mora_size: int  # モーラ数
+    chain_rule: str  # アクセント結合規則
+    chain_flag: int  # アクセント句の連結フラグ
